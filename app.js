0bde5e1f5acec1e01906155e041a6b0a9918f44b
lokesh62 <lokeshpentyala1999@gmail.com>
2021-11-03 17:13:29 -0500

    added selector

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var carRouter = require('./routes/car');
 var addmodsRouter = require('./routes/addmods');
+var selectorRouter = require('./routes/selector');
 
 
 var app = express();
@@ -26,6 +27,7 @@ app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/car', carRouter);
 app.use('/addmods', addmodsRouter);
+app.use('/selector', selectorRouter);
 
 
 // catch 404 and forward to error handler
--- dist/app.dev.js
+++ dist/app.dev.js
@@ -18,6 +18,8 @@ var carRouter = require('./routes/car');
 
 var addmodsRouter = require('./routes/addmods');
 
+var selectorRouter = require('./routes/selector');
+
 var app = express(); // view engine setup
 
 app.set('views', path.join(__dirname, 'views'));
@@ -32,7 +34,8 @@ app.use(express["static"](path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/car', carRouter);
-app.use('/addmods', addmodsRouter); // catch 404 and forward to error handler
+app.use('/addmods', addmodsRouter);
+app.use('/selector', selectorRouter); // catch 404 and forward to error handler
 
 app.use(function (req, res, next) {
   next(createError(404));
